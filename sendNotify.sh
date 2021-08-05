706017b2a9f4b4951517802fdd9d81ea62f285c3
Jefferson Braga <jeffbraga@gmail.com>
2021-08-05 10:39:48 -0300

    Fix: Try to make simple and change url

---
+++ sendNotify.sh
@@ -0,0 +1,12 @@
+text='{
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Action name: *Tag-or-Commit-action*\n> Repo: '${GITHUB_REPOSITORY}'\n> Workflow: '${GITHUB_WORKFLOW}'\n> Job: '${GITHUB_JOB}'\n> Executions number: '${GITHUB_RUN_NUMBER}'\n> Elapsed Time: '${elapsedTimeSeconds}' seconds\n> Labels: '${GITHUB_RUNNER_LABELS}'\n> Hostname: '${HOSTNAME}'\n> Workspace: '${GITHUB_WORKSPACE}'"
+                }
+            }
+        ]
+    }' 
+curl -v -X POST -H 'Content-type: application/json' --data "${text}" https://hooks.slack.com/services/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
